28763771eb23c7501563279982087e5a00094349
dgrisham <dgrisham@mines.edu>
2015-04-27 17:10:20 -0600

    added more comments

--- algobowl.py
+++ algobowl.py
@@ -3,7 +3,6 @@
 import sys
 
 def main(argv):
-    # num_tasks, num_machs, proc_times, speeds = get_inputs(argv)
     inputs = get_inputs(argv)
     greedy_method(inputs)
 
@@ -16,6 +15,9 @@ def greedy_method(inputs):
     times = [0 for i in range(num_machs)] # array to hold the process time for each machine
 
     for proc, proc_time in enumerate(proc_times):
+        # proc is the process number, proc_time is the process time
+
+        # make a greedy choice for every process
         processes, times = greedy_choice(proc, proc_time, times, processes, speeds)
 
     print_results(processes, times)
@@ -29,7 +31,8 @@ def greedy_choice(proc, proc_time, times, processes, speeds):
         times[mach] += speed * proc_time
         diff = max(times) - min(times)
 
-        if mach == 0:
+        # check if the current machine choice is the best choice so far
+        if mach == 0: # first machine, must be the best choice so far
             min_diff = diff
             best_choice = mach
         else:
@@ -37,17 +40,23 @@ def greedy_choice(proc, proc_time, times, processes, speeds):
                 min_diff = diff
                 best_choice = mach
 
+        # go back to the original process times so that we can test the next
+        # machine
         times[mach] -= speed * proc_time
 
+    # get the speed of the machine we've chosen
     speed = speeds[best_choice]
 
+    # update the machine we've chosen with the process
     processes[best_choice] += [proc]
+    # add the time that this process adds to the chosen machine's total time
     times[best_choice] += speed * proc_time
 
     return processes, times
 
 def get_inputs(argv):
     infile = argv[0]
+
     with open(infile) as f:
         inputs = [line.strip() for line in f]
 
