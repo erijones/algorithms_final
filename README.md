931f7304f67590667cdcac646afa9aca65ce218f
Eric <jones.eric93@gmail.com>
2015-04-27 21:55:14 -0700

    working top_swapper

--- README.md
+++ README.md
@@ -3,4 +3,9 @@ test commit
 
 to run the program, type something like: ./algobowl.py input.txt
 
-test commit
+try running ./algobowl.py piazza_input.txt
+it uses 100000 (by default) random swaps of a random element in the highest-time
+computer with a random element in a random computer
+
+on piazza, (piazza_input.txt) the highest they get is 9889.1875 and we can get
+9889.0 if you run it a couple of times or get lucky
--- algobowl.py
+++ algobowl.py
@@ -1,10 +1,76 @@
 #!/usr/bin/python
 
 import sys
+import random as rand
+import time
 
 def main(argv):
     inputs = get_inputs(argv)
-    greedy_method(inputs)
+
+    processes, times = greedy_method(inputs)
+    print("greedy min distance:")
+    print_results(processes, times)
+    print()
+
+    print("top swapper min distance:")
+    processes, times = top_swapper(processes, times, inputs)
+    print_results(processes, times)
+
+def top_swapper(processes, times, inputs):
+    # run the top swapper a bunch of times
+    tic = time.time()
+    num_swaps = 1000000
+    for i in range(num_swaps):
+        processes, times = top_swapper_once(processes, times, inputs)
+    toc = time.time()
+    print("time run: ", toc-tic, "s; # swaps: ", num_swaps)
+    return processes, times
+
+def top_swapper_once(processes, times, inputs):
+    num_tasks, num_machs, proc_times, speeds = inputs
+    # highest is mach with largest time; high_elem is the random element
+    # selected from that mach
+    highest = times.index(max(times))
+    high_elem = rand.randint(0, len(processes[highest])-1)
+
+    # swappee is randomly selected mach; swappee_elem is same as above
+    swappee = rand.randint(0, len(times)-1)
+    swap_elem = rand.randint(0, len(processes[swappee])-1)
+
+    # check relative distances
+    before_diff = times[highest] - times[swappee]
+    after_high_time = times[highest] \
+        - (proc_times[processes[highest][high_elem]] / speeds[highest]) \
+        + (proc_times[processes[swappee][swap_elem]] / speeds[highest])
+    after_swap_time = times[swappee] \
+        + (proc_times[processes[highest][high_elem]] / speeds[swappee]) \
+        - (proc_times[processes[swappee][swap_elem]] / speeds[swappee])
+    #after_diff = abs((times[highest] -
+    #                  (proc_times[processes[highest][high_elem]] / speeds[highest]) +
+    #                  (proc_times[processes[swappee][swap_elem]] / speeds[highest])) -
+    #                 (times[swappee] +
+    #                  (proc_times[processes[highest][high_elem]] / speeds[swappee]) -
+    #                  (proc_times[processes[swappee][swap_elem]] / speeds[swappee])))
+    after_diff = abs(after_high_time - after_swap_time)
+
+    #print(before_diff, after_diff)
+
+    # swap if beneficial
+    if before_diff <= after_diff:
+        return processes, times
+    else:
+        temp_high = processes[highest].pop(high_elem)
+        temp_swap = processes[swappee].pop(swap_elem)
+        processes[highest].append(temp_swap)
+        processes[swappee].append(temp_high)
+        times[highest] = after_high_time
+        times[swappee] = after_swap_time
+        return processes, times
+
+
+
+    return processes, times
+
 
 def greedy_method(inputs):
     # solves problem based on the greedy method
@@ -20,7 +86,8 @@ def greedy_method(inputs):
         # make a greedy choice for every process
         processes, times = greedy_choice(proc, proc_time, times, processes, speeds)
 
-    print_results(processes, times)
+    #print_results(processes, times)
+    return processes, times
 
 def greedy_choice(proc, proc_time, times, processes, speeds):
     # makes single choice for greedy method
@@ -70,9 +137,8 @@ def get_inputs(argv):
     return inputs
 
 def print_results(processes, times):
-    for mach in processes:
-        print(' '.join(str(proc) for proc in mach))
-        print()
+    #for mach in processes:
+    #    print(' '.join(str(proc) for proc in mach))
 
     print(max(times))
 
